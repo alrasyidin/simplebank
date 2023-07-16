d0cf3876994ae6223adee5bdf56233d47b272956
Hafidh Pradipta A <hamstergeek38@gmail.com>
2023-07-16 13:39:33 +0700

    refactor: load config with viper

--- main.go
+++ main.go
@@ -6,17 +6,17 @@ import (
 
 	"github.com/alrasyidin/simplebank-go/api"
 	db "github.com/alrasyidin/simplebank-go/db/sqlc"
+	"github.com/alrasyidin/simplebank-go/util"
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:postgres@localhost:5432/simple_bank?sslmode=disable"
-	address  = ":8081"
-)
-
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 
 	if err != nil {
 		log.Fatal("cannot connect to db", err)
@@ -25,7 +25,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(address)
+	err = server.Start(config.ServerAddress)
 
 	if err != nil {
 		log.Fatal("cannot start server", err)
